c8753acd2f3bef1fafd4998d10417e7db650e158
Siarhei Khonski <armonius@mail.ru>
2023-03-03 21:33:14 +0200

    init: init the project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        filename: "./src/js/index.js",
+    },
+    output: {
+        filename: "./src/js/index.js",
+        clean: true
+    },
+    devServer: {
+        watchFiles: ["index.html", "./src/js/*.js", "./src/css/*.css"],
+        port: 4000,
+        hot: true
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {from: "src/public", to: "./src/public"},
+                {from: "src/css/style.css", to: "./src/css"},
+                {from: "src/css/style.css.map", to: "./src/css"},
+                {from: "./index.html", to: ""}
+            ],
+        }),
+    ],
+};
