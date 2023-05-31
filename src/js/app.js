8b669b771cbb7c4b20b1ef44338ecfbd64a96f51
Siarhei Khonski <armonius@mail.ru>
2023-05-31 20:47:46 +0300

    feat: add change the amount of agents depentdently of the page's orientation

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,25 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import Header from "./header";
 import Agent from "./agent";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function App() {
-    const [agentState, setAgentState] = useState([false, false, false, false]);
+    const [agentSelectState, setAgentSelectState] = useState([false, false, false, false]);
+    const orientation = window.screen.orientation.type;
+    const isLandscape = orientation === "landscape-primary" || orientation === "landscape-secondary";
+    const [agentShowState, setAgentShowState] = useState(isLandscape);
+    function resizeHandler() {
+        const orientation = window.screen.orientation.type;
+        const isLandscape = orientation === "landscape-primary" || orientation === "landscape-secondary";
+        setAgentShowState(isLandscape);
+    }
+    ;
+    useEffect(() => {
+        function removeResList() {
+            window.removeEventListener('resize', resizeHandler);
+        }
+        ;
+        return removeResList;
+    });
     function agentClickHandler(event) {
         const parent = event.target.offsetParent;
         const newState = [];
@@ -12,9 +28,10 @@ export default function App() {
             newState.push(i === idNum);
         }
         ;
-        setAgentState(newState);
+        setAgentSelectState(newState);
     }
     ;
-    return (_jsxs("div", Object.assign({ className: "app" }, { children: [_jsx(Header, {}), _jsxs("div", Object.assign({ className: "app_agents-container", id: "app_agents-container", onClick: agentClickHandler }, { children: [_jsx(Agent, { id: "agent0", selected: agentState[0] }), _jsx(Agent, { id: "agent1", selected: agentState[1] }), _jsx(Agent, { id: "agent2", selected: agentState[2] }), _jsx(Agent, { id: "agent3", selected: agentState[3] })] }))] })));
+    window.addEventListener('resize', resizeHandler);
+    return (_jsxs("div", Object.assign({ className: "app" }, { children: [_jsx(Header, {}), _jsxs("div", Object.assign({ className: "app_agents-container", id: "app_agents-container", onClick: agentClickHandler }, { children: [_jsx(Agent, { id: "agent0", selected: agentSelectState[0] }), agentShowState ? _jsx(Agent, { id: "agent1", selected: agentSelectState[1] }) : '', agentShowState ? _jsx(Agent, { id: "agent2", selected: agentSelectState[2] }) : '', agentShowState ? _jsx(Agent, { id: "agent3", selected: agentSelectState[3] }) : ''] }))] })));
 }
 ;
--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -1,10 +1,26 @@
 import Header from "./header";
 import Agent from "./agent";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
 
-    const [agentState, setAgentState] = useState([false, false, false, false]);
+    const [agentSelectState, setAgentSelectState] = useState([false, false, false, false]);
+    const orientation = window.screen.orientation.type;
+    const isLandscape = orientation === "landscape-primary" || orientation === "landscape-secondary";
+    const [agentShowState, setAgentShowState] = useState(isLandscape);
+        
+    function resizeHandler() {
+      const orientation = window.screen.orientation.type;
+      const isLandscape = orientation === "landscape-primary" || orientation === "landscape-secondary";
+      setAgentShowState(isLandscape);
+    };
+
+    useEffect(() => { 
+      function removeResList() {
+        window.removeEventListener('resize', resizeHandler);
+      };
+      return removeResList;
+    });
 
     function agentClickHandler(event) {
       const parent = event.target.offsetParent;
@@ -13,16 +29,19 @@ export default function App() {
       for (let i = 0; i < 5; i += 1) {
         newState.push(i === idNum);
       };
-      setAgentState(newState);
-    };
+      setAgentSelectState(newState);
+    }; 
+    
+    window.addEventListener('resize', resizeHandler);
+
     return (
         <div className="app">
             <Header /> 
             <div className="app_agents-container" id="app_agents-container" onClick={agentClickHandler}>
-                <Agent id="agent0" selected={agentState[0]}/>
-                <Agent id="agent1" selected={agentState[1]}/>
-                <Agent id="agent2" selected={agentState[2]}/>
-                <Agent id="agent3" selected={agentState[3]}/>
+                <Agent id="agent0" selected={agentSelectState[0]}/>
+                { agentShowState ? <Agent id="agent1" selected={agentSelectState[1]}/> : '' }
+                { agentShowState ? <Agent id="agent2" selected={agentSelectState[2]}/> : '' }
+                { agentShowState ? <Agent id="agent3" selected={agentSelectState[3]}/> : '' }
             </div>  
         </div>
     );
