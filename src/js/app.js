69f7ed783b3d61f40eca1243ab2edf15af5a3afb
Siarhei Khonski <armonius@mail.ru>
2023-06-07 20:27:49 +0300

    refactor: refactor showing agents with the portrait orientation

--- src/js/app.js
+++ src/js/app.js
@@ -6,15 +6,16 @@ export default function App() {
     const [agentSelectState, setAgentSelectState] = useState([false, false, false, false]);
     const orientation = window.screen.orientation.type;
     const isLandscape = orientation === "landscape-primary" || orientation === "landscape-secondary";
-    const [agentShowState, setAgentShowState] = useState(isLandscape);
+    const [agentShowState, setAgentShowState] = useState([isLandscape, isLandscape, isLandscape, isLandscape]);
     function resizeHandler() {
         setAgentSelectState([false, false, false, false]);
         const orientation = window.screen.orientation.type;
         const isLandscape = orientation === "landscape-primary" || orientation === "landscape-secondary";
-        setAgentShowState(isLandscape);
+        setAgentShowState([true, isLandscape, isLandscape, isLandscape]);
     }
     ;
-    function swapHandler(event) {
+    function touchHandler(event) {
+        console.log(event);
     }
     ;
     useEffect(() => {
@@ -22,11 +23,12 @@ export default function App() {
             window.removeEventListener('resize', resizeHandler);
         }
         ;
-        function removeSwap() {
-            window.removeEventListener('touchmove', swapHandler);
+        function removeTouch() {
+            window.removeEventListener('touchstart', touchHandler);
+            window.removeEventListener('touchend', touchHandler);
         }
         ;
-        return () => { removeResList(); removeSwap(); };
+        return () => { removeResList(); removeTouch(); };
     });
     function agentClickHandler(event) {
         const parent = event.target.offsetParent;
@@ -40,7 +42,8 @@ export default function App() {
     }
     ;
     window.addEventListener('resize', resizeHandler);
-    window.addEventListener('touchmove', swapHandler);
-    return (_jsxs("div", Object.assign({ className: "app" }, { children: [_jsx(Header, {}), _jsxs("div", Object.assign({ className: "app_agents-container", id: "app_agents-container", onClick: agentClickHandler }, { children: [_jsx(Agent, { id: "agent0", selected: agentSelectState[0] }), agentShowState ? _jsx(Agent, { id: "agent1", selected: agentSelectState[1] }) : '', agentShowState ? _jsx(Agent, { id: "agent2", selected: agentSelectState[2] }) : '', agentShowState ? _jsx(Agent, { id: "agent3", selected: agentSelectState[3] }) : ''] }))] })));
+    window.addEventListener('touchstart', touchHandler);
+    window.addEventListener('touchend', touchHandler);
+    return (_jsxs("div", Object.assign({ className: "app" }, { children: [_jsx(Header, {}), _jsxs("div", Object.assign({ className: "app_agents-container", id: "app_agents-container", onClick: agentClickHandler }, { children: [agentShowState[0] ? _jsx(Agent, { id: "agent0", selected: agentSelectState[0] }) : '', agentShowState[1] ? _jsx(Agent, { id: "agent1", selected: agentSelectState[1] }) : '', agentShowState[2] ? _jsx(Agent, { id: "agent2", selected: agentSelectState[2] }) : '', agentShowState[3] ? _jsx(Agent, { id: "agent3", selected: agentSelectState[3] }) : ''] }))] })));
 }
 ;
--- src/ts/app.tsx
+++ src/ts/app.tsx
@@ -7,27 +7,28 @@ export default function App() {
     const [agentSelectState, setAgentSelectState] = useState([false, false, false, false]);
     const orientation = window.screen.orientation.type;
     const isLandscape = orientation === "landscape-primary" || orientation === "landscape-secondary";
-    const [agentShowState, setAgentShowState] = useState(isLandscape);
+    const [agentShowState, setAgentShowState] = useState([isLandscape, isLandscape, isLandscape, isLandscape]);
         
     function resizeHandler() {
       setAgentSelectState([false, false, false, false]);
       const orientation = window.screen.orientation.type;
       const isLandscape = orientation === "landscape-primary" || orientation === "landscape-secondary";
-      setAgentShowState(isLandscape);
+      setAgentShowState([true, isLandscape, isLandscape, isLandscape]);
     };
     
-    function swapHandler(event) {
-      
+    function touchHandler(event) {
+      console.log(event);
     };
 
     useEffect(() => { 
       function removeResList() {
         window.removeEventListener('resize', resizeHandler);
       };
-      function removeSwap() {
-        window.removeEventListener('touchmove', swapHandler);
+      function removeTouch() {
+        window.removeEventListener('touchstart', touchHandler);
+        window.removeEventListener('touchend', touchHandler);
       };
-      return () => { removeResList(); removeSwap(); };
+      return () => { removeResList(); removeTouch(); };
     });
 
     function agentClickHandler(event) {
@@ -41,16 +42,17 @@ export default function App() {
     }; 
     
     window.addEventListener('resize', resizeHandler);
-    window.addEventListener('touchmove', swapHandler);
+    window.addEventListener('touchstart', touchHandler);
+    window.addEventListener('touchend', touchHandler);
 
     return (
         <div className="app">
             <Header /> 
             <div className="app_agents-container" id="app_agents-container" onClick={agentClickHandler}>
-                <Agent id="agent0" selected={agentSelectState[0]}/>
-                { agentShowState ? <Agent id="agent1" selected={agentSelectState[1]}/> : '' }
-                { agentShowState ? <Agent id="agent2" selected={agentSelectState[2]}/> : '' }
-                { agentShowState ? <Agent id="agent3" selected={agentSelectState[3]}/> : '' }
+                { agentShowState[0] ? <Agent id="agent0" selected={agentSelectState[0]}/> : '' }
+                { agentShowState[1] ? <Agent id="agent1" selected={agentSelectState[1]}/> : '' }
+                { agentShowState[2] ? <Agent id="agent2" selected={agentSelectState[2]}/> : '' }
+                { agentShowState[3] ? <Agent id="agent3" selected={agentSelectState[3]}/> : '' }
             </div>  
         </div>
     );
